f32836bcc08b533fd6a52ac4eda52ffb1e89eacb
BirjuVachhani <brvachhani@gmail.com>
2018-06-19 18:18:59 +0530

    Implemented:
    - Solve codacy issues.
    - Added data binding
    - Remove auto generated test files

--- circular_reveal_effect/src/main/java/com/birjuvachhani/circular_reveal_effect/RevealEffectActivity.java
+++ circular_reveal_effect/src/main/java/com/birjuvachhani/circular_reveal_effect/RevealEffectActivity.java
@@ -1,84 +1,73 @@
 package com.birjuvachhani.circular_reveal_effect;
 
 import android.animation.Animator;
+import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.constraint.Group;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.widget.ImageView;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import com.birjuvachhani.circular_reveal_effect.databinding.ActivityRevealEffectBinding;
 
 public class RevealEffectActivity extends AppCompatActivity implements View.OnTouchListener {
 
     private boolean isInfoVisible = false;
+    private ActivityRevealEffectBinding activityRevealEffectBinding;
 
-    @BindView(R.id.albumInfoContainer)
-    View albumInfoContainer;
-
-    @BindView(R.id.ivAlbumArt)
-    View albumArt;
-
-    @BindView(R.id.textGroup)
-    Group textGroup;
-
-    private int hypotenuse;
     private int pointX;
     private int pointY;
     private Animator.AnimatorListener revealListener = new Animator.AnimatorListener() {
         @Override
         public void onAnimationStart(Animator animation) {
-
+            // Nothing to do
         }
 
         @Override
         public void onAnimationEnd(Animator animation) {
-            textGroup.setVisibility(View.VISIBLE);
-            textGroup.startAnimation(mAlphaAnim);
+            activityRevealEffectBinding.textGroup.setVisibility(View.VISIBLE);
+            activityRevealEffectBinding.textGroup.startAnimation(mAlphaAnim);
         }
 
         @Override
         public void onAnimationCancel(Animator animation) {
-
+            // Nothing to do
         }
 
         @Override
         public void onAnimationRepeat(Animator animation) {
-
+            // Nothing to do
         }
     };
 
     private Animator.AnimatorListener unravelListener = new Animator.AnimatorListener() {
         @Override
         public void onAnimationStart(Animator animation) {
-
+            // Nothing to do
         }
 
         @Override
         public void onAnimationEnd(Animator animation) {
-            albumInfoContainer.setVisibility(View.GONE);
-            textGroup.setVisibility(View.GONE);
+            activityRevealEffectBinding.albumInfoContainer.setVisibility(View.GONE);
+            activityRevealEffectBinding.textGroup.setVisibility(View.GONE);
         }
 
         @Override
         public void onAnimationCancel(Animator animation) {
-
+            // Nothing to do
         }
 
         @Override
         public void onAnimationRepeat(Animator animation) {
-
+            // Nothing to do
         }
     };
 
@@ -87,28 +76,27 @@ public class RevealEffectActivity extends AppCompatActivity implements View.OnTo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reveal_effect);
-        ButterKnife.bind(this);
-        albumArt.setOnTouchListener(this);
+        activityRevealEffectBinding = DataBindingUtil.setContentView(this, R.layout.activity_reveal_effect);
+        activityRevealEffectBinding.ivAlbumArt.setOnTouchListener(this);
         mAlphaAnim = new AlphaAnimation(0.0f, 1.0f);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void showMoreInfo(View view) {
-        hypotenuse = (int) Math.hypot(albumArt.getWidth(), albumArt.getHeight());
+        int hypotenuse = (int) Math.hypot(activityRevealEffectBinding.ivAlbumArt.getWidth(), activityRevealEffectBinding.ivAlbumArt.getHeight());
         if (isInfoVisible) {
-            Animator animator = ViewAnimationUtils.createCircularReveal(albumInfoContainer, pointX, pointY, hypotenuse, 0);
+            Animator animator = ViewAnimationUtils.createCircularReveal(activityRevealEffectBinding.albumInfoContainer, pointX, pointY, hypotenuse, 0);
             animator.setDuration(400);
             animator.addListener(unravelListener);
             animator.start();
             isInfoVisible = false;
             return;
         }
-        albumInfoContainer.setBackgroundColor(getTouchedColor());
-        Animator animator = ViewAnimationUtils.createCircularReveal(albumInfoContainer, pointX, pointY, 0, hypotenuse);
+        activityRevealEffectBinding.albumInfoContainer.setBackgroundColor(getTouchedColor());
+        Animator animator = ViewAnimationUtils.createCircularReveal(activityRevealEffectBinding.albumInfoContainer, pointX, pointY, 0, hypotenuse);
         animator.setDuration(400);
         animator.addListener(revealListener);
-        albumInfoContainer.setVisibility(View.VISIBLE);
+        activityRevealEffectBinding.albumInfoContainer.setVisibility(View.VISIBLE);
         animator.start();
         isInfoVisible = true;
     }
@@ -117,13 +105,13 @@ public class RevealEffectActivity extends AppCompatActivity implements View.OnTo
         float[] eventXY = new float[]{pointX, pointY};
 
         Matrix invertMatrix = new Matrix();
-        ((ImageView) albumArt).getImageMatrix().invert(invertMatrix);
+        activityRevealEffectBinding.ivAlbumArt.getImageMatrix().invert(invertMatrix);
 
         invertMatrix.mapPoints(eventXY);
         int x = (int) eventXY[0];
         int y = (int) eventXY[1];
 
-        Drawable imgDrawable = ((ImageView) albumArt).getDrawable();
+        Drawable imgDrawable = activityRevealEffectBinding.ivAlbumArt.getDrawable();
         Bitmap bitmap = ((BitmapDrawable) imgDrawable).getBitmap();
         return bitmap.getPixel(x, y);
     }
